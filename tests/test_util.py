dec3805dec1ec586a369a2285e71fd6c0a832f07
Talia Kimber <talia.kimber@gmail.com>
2021-01-15 13:42:18 +0100

    :white_check_mark: Update tests.

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,11 +1,11 @@
 import pytest
-from util import my_util_function
+from util import return_first_arg, return_second_arg
 
 ####################
 @pytest.mark.parametrize(
     "arg1, arg2, solution",
     [(1, 2, 1), (0, 0, 0)],
 )
-def test_my_util_function(arg1, arg2, solution):
-    my_sol = my_util_function(arg1, arg2)
+def test_return_first_arg(arg1, arg2, solution):
+    my_sol = return_first_arg(arg1, arg2)
     assert solution == my_sol
